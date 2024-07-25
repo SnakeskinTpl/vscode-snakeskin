578592bee2026117c278ad7d366121aeb3ad0231
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-07-25 14:11:00 +0300

    Modify semantic token for tag if it's HTML

--- packages/language/src/semantic-tokens.ts
+++ packages/language/src/semantic-tokens.ts
@@ -2,14 +2,17 @@ import type { AstNode } from 'langium';
 import { AbstractSemanticTokenProvider, type SemanticTokenAcceptor } from 'langium/lsp';
 import { SemanticTokenTypes, SemanticTokenModifiers } from 'vscode-languageserver-protocol';
 import { isAttribute, isClass, isTag } from './generated/ast.js';
+import { getDefaultHTMLDataProvider } from 'vscode-html-languageservice/lib/esm/htmlLanguageService.js';
 
 export class SemanticTokenProvider extends AbstractSemanticTokenProvider {
 	protected override highlightElement(node: AstNode, acceptor: SemanticTokenAcceptor): void | 'prune' | undefined {
 		if (isTag(node)) {
+			const isNativeHTML = getDefaultHTMLDataProvider().provideTags().some(tag => tag.name.toLowerCase() === node.tagName?.toLowerCase());
 			acceptor({
 				node,
 				property: 'tagName',
 				type: SemanticTokenTypes.function,
+				modifier: isNativeHTML ? [SemanticTokenModifiers.defaultLibrary] : [],
 			});
 		} else if (isAttribute(node)) {
 			acceptor({
