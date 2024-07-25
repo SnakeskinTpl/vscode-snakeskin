4fb720e51149d9f1b427f9716ff3b63a6aa13a08
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-07-25 12:59:25 +0300

    Fix paths in CLI
    It was fixed in the template after this repo was created already

--- packages/cli/src/generator.ts
+++ packages/cli/src/generator.ts
@@ -1,17 +1,15 @@
-import type { Model } from 'snakeskin-language';
+import type { Module } from 'snakeskin-language';
 import { expandToNode, joinToNode, toString } from 'langium/generate';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { extractDestinationAndName } from './util.js';
 
-export function generateJavaScript(model: Model, filePath: string, destination: string | undefined): string {
+export function generateJavaScript(module: Module, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
     const generatedFilePath = `${path.join(data.destination, data.name)}.js`;
 
     const fileNode = expandToNode`
         "use strict";
-
-        ${joinToNode(model.greetings, greeting => `console.log('Hello, ${greeting.person.ref?.name}!');`, { appendNewLineIfNotEmpty: true })}
     `.appendNewLineIfNotEmpty();
 
     if (!fs.existsSync(data.destination)) {
--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -1,4 +1,4 @@
-import type { Model } from 'snakeskin-language';
+import type { Module } from 'snakeskin-language';
 import { createSnakeskinServices, SnakeskinLanguageMetaData } from 'snakeskin-language';
 import chalk from 'chalk';
 import { Command } from 'commander';
@@ -10,12 +10,12 @@ import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
+const packagePath = path.resolve(__dirname, '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const services = createSnakeskinServices(NodeFileSystem).Snakeskin;
-    const model = await extractAstNode<Model>(fileName, services);
+    const model = await extractAstNode<Module>(fileName, services);
     const generatedFilePath = generateJavaScript(model, fileName, opts.destination);
     console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
 };
