e45b80d0275e3ecb4d790fd089d5d2ab13c0def8
Abdelrahman Abounegm <abounegm@yandex-team.ru>
2024-08-05 19:04:09 +0300

    Add file and extension icons

---
+++ packages/intellij-plugin/src/main/kotlin/snakeskin/lang/SnakeskinIcons.kt
@@ -0,0 +1,8 @@
+package snakeskin.lang
+
+import com.intellij.openapi.util.IconLoader
+
+object SnakeskinIcons {
+    @JvmField
+    val Logo = IconLoader.getIcon("/icons/snakeskin-logo.svg", javaClass)
+}
